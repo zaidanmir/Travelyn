1a487a17e8b61d3b926a390ecdd94eee9a4a9bcb
zaidanmir <zaidan2440@gmail.com>
2025-12-28 20:10:40 +0000

    Initial Commit

---
+++ Travelyn/TravelynApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelynApp.swift
+//  Travelyn
+//
+//  Created by Zaidan Mir on 28/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct TravelynApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
